85f01abd6999528b510645f99903a574a97ae530
adam buxton <abuxton@users.noreply.github.com>
2020-04-11 15:19:22 +0100

    update bootstrap script

--- user_data.sh
+++ user_data.sh
@@ -41,7 +41,7 @@ ubuntu_linux_setup() {
   export SSH_USER="ubuntu"
   export DEBIAN_FRONTEND=noninteractive
   /usr/bin/apt-get update
-  /usr/bin/apt-get -yq install -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" default-jre wget awscli
+  /usr/bin/apt-get -yq install -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" default-jre wget awscli openjdk-8-jre-headless curl screen nano bash grep tree 
   /bin/cat <<"__UPG__" > /etc/apt/apt.conf.d/10periodic
 APT::Periodic::Update-Package-Lists "1";
 APT::Periodic::Download-Upgradeable-Packages "1";
@@ -49,40 +49,66 @@ APT::Periodic::AutocleanInterval "7";
 APT::Periodic::Unattended-Upgrade "1";
 __UPG__
 
-  # Init script for starting, stopping
-  cat <<INIT > /etc/init.d/minecraft
-#!/bin/bash
-# Short-Description: Minecraft server
+adduser --system --shell /bin/bash --home /opt/minecraft --group minecraft
 
-start() {
-  echo "Starting minecraft server from /home/minecraft..."
-  start-stop-daemon --start --quiet  --pidfile ${mc_root}/minecraft.pid -m -b -c $SSH_USER -d ${mc_root} --exec /usr/bin/java -- -Xmx${java_mx_mem} -Xms${java_ms_mem} -jar $MINECRAFT_JAR nogui
-}
+# Init script for starting, stopping https://minecraft.gamepedia.com/Tutorials/Server_startup_script#Systemd_Script
+  cat <<INIT > /etc/systemd/system/minecraft@.service
+# Source: https://github.com/agowa338/MinecraftSystemdUnit/
+# License: MIT
+[Unit]
+Description=Minecraft Server %i
+After=network.target
 
-stop() {
-  echo "Stopping minecraft server..."
-  start-stop-daemon --stop --pidfile ${mc_root}/minecraft.pid
-}
+[Service]
+WorkingDirectory=/opt/minecraft/%i
+PrivateUsers=true 
+# Users Database is not available for within the unit, only root and minecraft is available, everybody else is nobody
+User=minecraft
+Group=minecraft
+ProtectSystem=full 
+# Read only mapping of /usr /boot and /etc
+ProtectHome=true 
+# /home, /root and /run/user seem to be empty from within the unit. It is recommended to enable this setting for all long-running services (in particular network-facing ones).
+ProtectKernelTunables=true 
+# /proc/sys, /sys, /proc/sysrq-trigger, /proc/latency_stats, /proc/acpi, /proc/timer_stats, /proc/fs and /proc/irq will be read-only within the unit. It is recommended to turn this on for most services.
+# Implies MountFlags=slave
+ProtectKernelModules=true 
+# Block module system calls, also /usr/lib/modules. It is recommended to turn this on for most services that do not need special file systems or extra kernel modules to work
+# Implies NoNewPrivileges=yes
+ProtectControlGroups=true 
+# It is hence recommended to turn this on for most services.
+# Implies MountAPIVFS=yes
 
-case \$1 in
-  start)
-    start
-    ;;
-  stop)
-    stop
-    ;;
-  restart)
-    stop
-    sleep 5
-    start
-    ;;
-esac
-exit 0
+ExecStart=/bin/sh -c '/usr/bin/screen -DmS mc-%i /usr/bin/java -server -Xms512M -Xmx2048M -XX:+UseG1GC -XX:+CMSIncrementalPacing -XX:+CMSClassUnloadingEnabled -XX:ParallelGCThreads=2 -XX:MinHeapFreeRatio=5 -XX:MaxHeapFreeRatio=10 -jar $(ls -v | grep -i "FTBServer.*jar\|minecraft_server.*jar" | head -n 1) nogui'
+
+ExecReload=/usr/bin/screen -p 0 -S mc-%i -X eval 'stuff "reload"\\015'
+
+ExecStop=/usr/bin/screen -p 0 -S mc-%i -X eval 'stuff "say SERVER SHUTTING DOWN. Saving map..."\\015'
+ExecStop=/usr/bin/screen -p 0 -S mc-%i -X eval 'stuff "save-all"\\015'
+ExecStop=/usr/bin/screen -p 0 -S mc-%i -X eval 'stuff "stop"\\015'
+ExecStop=/bin/sleep 10
+
+Restart=on-failure
+RestartSec=60s
+
+[Install]
+WantedBy=multi-user.target
+
+#########
+# HowTo
+#########
+#
+# Create a directory in /opt/minecraft/XX where XX is a name like 'survival'
+# Add minecraft_server.jar into dir with other conf files for minecraft server
+#
+# Enable/Start systemd service
+#    systemctl enable minecraft@survival
+#    systemctl start minecraft@survival
+#
+# To run multiple servers simply create a new dir structure and enable/start it
+#    systemctl enable minecraft@creative
+# systemctl start minecraft@creative
 INIT
-
-  # Start up on reboot
-  /bin/chmod +x /etc/init.d/minecraft
-  /usr/sbin/update-rc.d minecraft defaults
 
 }
 
@@ -157,7 +183,7 @@ EULA
 # Start the server
 case $OS in
   Ubuntu*)
-    /etc/init.d/minecraft start
+    /usr/bin/systemctl start minecraft
     ;;
   Amazon*)
     /usr/bin/systemctl start minecraft
